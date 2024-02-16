1f59d0a41d2558ba0d44a4d902017db4c4f1bb9a
jasmina8835 <sunnatovajasmina8002@gmail.com>
2024-02-16 19:53:38 +0500

    topics

---
+++ script.js
@@ -0,0 +1,384 @@
+// ? outputs
+// console.log("5")
+// document.write("Hello")
+
+// alert("this is warning")
+
+// let headOne = document.querySelector('h1')
+
+// headOne.innerHTML = "this is head one"
+
+
+
+// 
+// alert("sahifaga kirdingiz")
+
+// let firstNumber = +prompt("birinchi sonni kiriting")
+// let operation = prompt("operatsiyani kiriting + - * /")
+
+// let secondNumber = +prompt("ikkinchi sonni kiriting")
+
+// let result;
+
+// if (operation == "+"){
+
+//     result = firstNumber + secondNumber
+// }
+//  else if (operation == "-"){
+//     result = firstNumber - secondNumber
+
+//  }else if (operation == "*"){
+//     result = firstNumber * secondNumber
+
+//  }else if (operation == "/") {
+//    result = firstNumber / secondNumber
+
+//  }else {
+//     alert("error")
+//  }
+
+//  if (result != underfined) {
+//     alert("the result is + result")
+//  }
+
+
+
+//  let demo = confirm("yoshingiz18 da kattami")
+
+//  console.log(demo);
+
+//  if (demo == true) {
+//     console.log("dostup bor)");
+//  }
+
+
+
+
+
+// ? Variable
+
+// var age = "salom"
+// var age = "alik"
+
+// console.log(age);
+
+
+// let surname = "Muradov"
+
+// let surnames = "Ashurov"
+
+// var global scope
+
+// if (true) {
+//     // blocked scope
+//     var demo = "salom info"
+//     let test = "this is let"
+
+//     let surname = "Botirov"
+// }
+
+// let na,e = "Shaxzod"
+
+// name = "bekzod"
+
+// console.log(name);
+
+
+
+
+// let headOne = document.querySelector('h1')
+// let minus = document.querySelector('.minus')
+// let plus = document.querySelector('.plus')
+// let num = 5;
+// headOne.innerHTML = num;
+
+// plus.addEventListener('click', ()=> {
+//     num +=1
+//     headOne.innerHTML = num;
+// })
+
+
+// minus.addEventListener('click', ()=> {
+//     num -=1
+//     headOne.innerHTML = num;
+// })
+
+
+
+// const weight = 75;
+
+// weight = 90;
+
+
+// console.log(weight);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("salom dunyo");
+
+// let age = "dfghjkloiuyt";
+
+
+
+// ? CamelCase
+// let colorCar = "red";
+
+
+// Data types
+// Malumot turlari
+
+// String
+// let str = "Salom"
+// let str2 = 'Dunyo'
+
+// let str3 = 'Nima gap'
+
+// let longStr = 'mening ismim "shaxzod"'
+
+
+
+// ? Number
+// let firstNumber = 4
+// let secondNum = 10
+
+// let strNum = 4
+
+
+// ? Boolean
+// let isTrue = true
+// let isFalse = false
+
+
+// ? underfunded
+
+// let und;
+
+// console.log(und);
+
+
+//  ? Null
+// let isNull = null;
+
+// console.log(isNull);
+
+
+// ? typeof
+// console.log(typeof isTrue);
+
+
+// ?Object
+
+// let person = {
+//     name: "Shaxzod",
+//     age: 25,
+//     weight: 100,
+//     height: 1.80,
+// }
+
+// let phone = {
+//     model: "iphone",
+//     color: "red",
+//     weight: 200,
+// }
+
+
+// ? object
+// ? array
+
+// let arr = [1,5,7,3,6,8]
+
+// console.log(typeof arr)
+
+
+// console.log(shaxzod.weight+"kg");
+// console.log(5*5);
+// document.body.innerHTML = firstNumber
+
+
+
+
+// ? Operatorlar
+
+// let a = 30;
+
+// a = 40;
+
+// let b = 10;
+
+
+// ? + Addition
+// let c = a + b;
+
+// let str1 = "salom";
+
+// let strUnd = "nimadir";
+
+// console.log(str1 - strUnd);
+
+
+// ? - Minus
+
+// console.log("result", a -b);
+
+
+// ? * Multiplication
+
+// let resMultiple = a * b;
+
+// console.log(resMultiple);
+
+
+// ? / division
+
+// let div = a / b;
+
+// console.log(div);
+
+
+// ? qoldiqli bolish
+
+// console.log(45 % b);
+
+// let d = 30;
+
+//  d = d + a; === d += a;
+
+// d += a;  // d = d+a;
+
+
+//  string
+// let z = 20;
+
+// number
+// let x = 20;
+
+// ==
+// ===
+
+// ? ! "emas" manosida keladi
+
+
+// console.log(z < x);
+// console.log(z !== x);
+
+// ? darajaga kotarish
+// console.log(z**2);
+
+
+
+
+// 09.01.24
+
+
+
+
+
+
+// let car = {
+//     name: "malibu" ,
+//     year: 2020 ,
+//     color: "black" ,
+// }
+// console.log(car);
+
+
+
+
+
+// let a = 5;
+// let b = 5;
+
+// console.log(a == b);
+
+
+// let a = 7;
+// let b = 4;
+
+// console.log(a == 7);
+
+
+// let b = 6;
+
+// console.log( b>=0);
+
+
+// let a = -6;
+
+// console.log( a<=0);
+
+
+// let a = 7;
+
+// console.log(a % 2 === 1);
+
+
+// let a = 8;
+
+// console.log(a % 2 === 0);
+
+
+
+// let a = 3;
+// let b = 4;
+// let c = 5;
+
+// console.log(b>a && b<c);
+
+
+
+// let a = 11;
+// let b = 16;
+
+// console.log(a>=0 && b>= 0);
+
+
+
+let a = 4;
+let b = 9;
+
+console.log(a % 2 === 0 && b % 2 === 0);
+
+
+// ? Function
+
+function sayHello(a, b) {
+    let result = a + b * 10;
+
+    return result;
+}
+
+console.log(sayHello(12, 34));
+
+
+
+// || yoki
+// && va
+
+console.log(true || false);
+
+let x = 10;
+let y = 15;
+let z = 30;
+
+console.log(x === 10 || y === 5 || z === 1);
+console.log(x === 10 && y === 15 && z ===30);
+
+
+function sayHello(x, y, z) {
+    let result = x + y + z
+
+    return result
+}
+
+console.log(sayHello(10, 15, 30));
+
